d45a2d93d849922253f4949a288e8607a46e0f7f
Brad Davidson <bdavidson@pivotal.io>
2020-03-04 10:40:51 -0700

    Migration Start

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -1,24 +1,86 @@
 #!/usr/bin/env bash
 set -e
 
+app_name="${1:-pal-tracker}"
+script_dir="${2:-.}"
+service_name="${3:-tracker-database}"
+service_key="${4:-flyway-migration-key}"
 
-app_guid=`cf app $1 --guid`
-credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES | if .["p-mysql"] != null then .["p-mysql"] elif .["p.mysql"] != null then .["p.mysql"] else .["cleardb"] end | .[0].credentials'`
+function main() {
+
+    echo "Creating service key, if necessary ..."
+
+    cf create-service-key $service_name $service_key > /dev/null
+
+    echo "Retrieving target database parameters ..."
+
+    credentials=$(cf service-key $service_name $service_key | sed -ne '/{/,$p')
+
+    db_host=$(echo $credentials | jq -r '.hostname')
+    db_name=$(echo $credentials | jq -r '.name')
+    db_username=$(echo $credentials | jq -r '.username')
+    db_password=$(echo $credentials | jq -r '.password')
+    db_port=$(echo $credentials | jq -r '.port')
+
+    test -n "$db_host" || exit 1
+
+    echo "Opening ssh tunnel to $db_host:$db_port ..."
+
+    cf ssh -N -L 63306:$db_host:$db_port $app_name &
+    cf_ssh_pid=$!
+
+    echo "Waiting for tunnel ..."
+
+    wait_for_tunnel
+
+    echo "Running migration ..."
+
+    flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" \
+        -locations=filesystem:"$script_dir"/databases/tracker \
+        -user="$db_username" \
+        -password="$db_password" \
+        migrate
+
+    echo "Migration complete"
+
+}
+
+function pre_tunnel_exit() {
+    echo "ERROR: No suitable credentials found for application '$app_name' and service '$service_name'" >&2
+}
+
+trap pre_tunnel_exit EXIT
+
+function close_tunnel_at_exit() {
+    echo "Closing tunnel"
+    kill -KILL $cf_ssh_pid
+}
+
+trap close_tunnel_at_exit EXIT
 
 
-ip_address=`echo $credentials | jq -r '.hostname'`
-db_name=`echo $credentials | jq -r '.name'`
-db_username=`echo $credentials | jq -r '.username'`
-db_password=`echo $credentials | jq -r '.password'`
+function wait_for_tunnel() {
 
-echo "Opening ssh tunnel to $ip_address"
-cf ssh -N -L 63306:$ip_address:3306 $1 &
-cf_ssh_pid=$!
+    set +e
 
-echo "Waiting for tunnel"
-sleep 5
+    for x in {1..10}; do
+        $(nc -z localhost 63306)
+        nc_status=$?
+        if [[ "$nc_status" == "0" ]]; then
+            echo "Ssh tunnel success."
+            break
+        else
+            echo "Ssh tunnel not detected yet..."
+            sleep 3
+        fi
+    done
 
-# Passing this in as a param is a bit strage. Maybe put flyway on the path?
-$3/flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" -locations=filesystem:$2/databases/tracker -user=$db_username -password=$db_password migrate
+    if [[ "$nc_status" != "0" ]] ; then
+        echo "Failed to detect ssh tunnel. Exiting."
+        exit 1
+    fi
 
-kill -STOP $cf_ssh_pid
+    set -e
+}
+
+main
